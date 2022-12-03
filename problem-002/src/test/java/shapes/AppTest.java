8339b016ec8a42cdef9ac0801b2fcc19dced08ee
SanghaviKA <sangaviashwath@gmail.com>
2022-12-03 23:23:57 +0530

    Testcase

--- problem-002/src/test/java/shapes/AppTest.java
+++ problem-002/src/test/java/shapes/AppTest.java
@@ -1,12 +1,40 @@
-package deduplication;
+package shapes;
 
 import org.junit.jupiter.api.*;
 
 class AppTest {
 
   @Test
-  public void testExample() {
-    Assertions.assertTrue(true);
+  public void testSquareWithRightValue() {
+    Square sq = new Square(10.0);
+    double area = sq.area();
+    double perimeter = sq.perimeter();
+    Assertions.assertEquals(100, area);
+    Assertions.assertEquals(40, perimeter);
+
+  }
+@Test
+  public void testSquereWithInvalidValues(){
+    Square sq = new Square(0);
+    double area = sq.area();
+    double perimeter = sq.perimeter();
+    Assertions.assertEquals(0, area);
+    Assertions.assertEquals(0, perimeter);
+    // Assertions.assertTrue(true);
+
+
+
+
+
+  }
+@Test
+  public void testRectWithNagativeValues(){
+    Rectangle rect = new Rectangle(-1, -10);
+    double area = rect.area();
+    double perimeter = rect.perimeter();
+    Assertions.assertEquals(-2, area);
+    Assertions.assertEquals(-2, perimeter);
+    // Assertions.assertTrue(true);
   }
 
 }
